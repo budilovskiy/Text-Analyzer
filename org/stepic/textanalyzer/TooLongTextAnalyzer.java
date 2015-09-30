fbc4fd42439dd22f4a033bf7972a125a3ac9d9d8
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-09-30 21:03:55 +0300

    Create TooLongTextAnalyzer.java

---
+++ org/stepic/textanalyzer/TooLongTextAnalyzer.java
@@ -0,0 +1,16 @@
+package org.stepic.textanalyzer;
+
+public class TooLongTextAnalyzer implements TextAnalyzer {
+	
+	private int maxLength;
+	
+	public TooLongTextAnalyzer(int maxLength) {
+		this.maxLength = maxLength;
+	}
+	
+	@Override
+	public Label processText(String text) {
+		return (text.length() > maxLength) ? Label.TOO_LONG : Label.OK;
+	}
+
+}
